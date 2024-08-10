5325651a1e6e3f39f726ace9ee63eb9fa829106e
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2024-08-09 19:32:09 -0700

    added pyinstaller spec to repo

--- build.bat
+++
@@ -1,1 +0,0 @@
-pyinstaller --noconfirm --onedir --console --name "llmcord" --add-data ".\plugins;plugins/" --icon "llmcord_logo.ico" "entry_point.py"
--- build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-pyinstaller --noconfirm --onedir --console --name "llmcord" --add-data "./plugins;plugins/" --icon "llmcord_logo.ico" "entry_point.py"
